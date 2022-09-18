18e3a4740c80baaa28753e5be3b32595b43bc980
andreas.sateras <andreas@sateras.se>
2022-09-18 18:20:02 +0200

    GraphGenerator2

---
+++ GraphLayout/tools/WpfGraphControl/NodeControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfGraphControl {
+    public class NodeControl : Control {
+        public object Node {
+            get { return GetValue(NodeProperty); }
+            set { SetValue(NodeProperty, value); }
+        }
+
+        public static readonly DependencyProperty NodeProperty =
+            DependencyProperty.Register("Node", typeof(object), typeof(NodeControl), new UIPropertyMetadata(null));
+    }
+}
